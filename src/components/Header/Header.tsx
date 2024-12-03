1dca6e4363c42632fc9dfdce25de21aeb33e1003
Lucas Pacheco <lucaspdroz@gmail.com>
2024-12-03 17:36:22 -0300

    feat:consuming store

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import './style.css';
 import img from '../../assets/150.png';
+import useStore from 'host/store'
 
 const Header: React.FC = () => {
+    const { count, clear } = useStore()
+
     return (
         <header className='header'>
             <div className="elements">
-                <p>Header tsx</p>
+                <p>Header{count}</p>
+                <button onClick={clear}>clear store</button>
                 <img src={img} alt="avatar" />
             </div>
         </header>
